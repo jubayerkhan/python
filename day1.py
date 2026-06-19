f5d339a4bc6b51b017d5cfdf40ee54645cf22c79
@jubayer.khan <jubayer.khan@annanovas.com>
2026-06-19 12:25:20 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+name = "Jubayer"
+age = 26
+cgpa = 3.36
+is_developer = True
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My cgpa is {cgpa}")
+print(f"Developer: {is_developer}")
---
+++ hello.py
@@ -0,0 +1,5 @@
+name = "Jubayer"
+age = 26
+
+print(f"My name is {name}")
+print(f"I am {age} years old")
